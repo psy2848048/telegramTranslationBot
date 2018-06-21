325037901260bfe6bef847b0b49727177ee59808
Bryan <junhang.lee@ciceron.me>
2018-06-21 14:03:42 +0900

    Polling in python

--- translatorBotGeneral.py
+++ translatorBotGeneral.py
@@ -1,6 +1,13 @@
 from translatorBot import TranslatorBot
+import time, traceback
 
 if __name__ == "__main__":
     translator = TranslatorBot()
-    translator.generalTranslation()
+    while True:
+        try:
+            translator.generalTranslation()
+        except:
+            traceback.print_exc()
+
+        time.sleep(0.5)
 
